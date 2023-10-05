2907916f2394881d6ca98edb7f408b4029053537
HANSEOL <nothinonthere@icoud.com>
2023-10-05 11:33:24 +0900

    [init]

---
+++ Client/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+
+int main (int argc, char const* argv[]) {
+    return 0;
+}
---
+++ Server/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#define PORT 8080
+
+int main(int argc, char const* argv[]) {
+    int server_fd, new_socket, valread;
+    
+    return 0;
+}
